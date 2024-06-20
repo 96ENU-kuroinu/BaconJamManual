e86b15bf427600d15eed9f05851ed7a58375e5e0
96ENU <96enu.illustration@gmail.com>
2024-06-20 12:56:51 +0900

    「Misskeyとの連携」ページを作成中…。

---
+++ docs/js/generate-toc.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const tocList = document.getElementById('toc-list');
+    const h2Elements = document.querySelectorAll('h2');
+
+    if (h2Elements) {
+        h2Elements.forEach((h2, index) => {
+            // 各h2にIDを追加
+            const id = `section-${index + 1}`;
+            h2.id = id;
+
+            // リストアイテムとリンクを生成
+            const li = document.createElement('li');
+            const a = document.createElement('a');
+            a.href = `#${id}`;
+            a.textContent = h2.textContent;
+
+            // リストアイテムにリンクを追加し、目次に追加
+            li.appendChild(a);
+            tocList.appendChild(li);
+        });
+    }
+});
