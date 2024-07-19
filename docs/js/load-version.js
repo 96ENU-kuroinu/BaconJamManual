9f66fbd048cd351db638766587dbbebe590cf08a
96ENU <96enu.illustration@gmail.com>
2024-07-19 12:55:49 +0900

    「バージョン履歴」ページを作成中…

--- docs/js/load-version.js
+++ docs/js/load-version.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     const versionElement = document.getElementById('version');
     if (versionElement) {
-        versionElement.textContent = "(Ver 1.4.8)";
+        versionElement.textContent = "(Ver 1.5.0)";
     }
 });
