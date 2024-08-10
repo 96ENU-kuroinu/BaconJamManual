576db78847753e8f12126caca27b56b5bf4e961d
96ENU <96enu.illustration@gmail.com>
2024-08-10 11:12:11 +0900

    フッタ動的生成のテスト

---
+++ docs/js/load-footer.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var footerElement = document.getElementById('footer');
+    footerElement.style.backgroundColor = '#95cf4a';
+    footerElement.style.padding = '10px';
+
+    // pタグを作成
+    const p = document.createElement('p');
+    p.style.justifyContent = 'center';
+    p.style.alignItems = 'center';
+    p.style.display = 'flex';
+
+    // 最初のテキストノードを作成してpタグに追加
+    const text1 = document.createTextNode('©');
+    p.appendChild(text1);
+
+    // aタグを作成
+    const a = document.createElement('a');
+    a.href = 'https://misskey.io/@96ENU'; // リンク先のURLを設定
+
+    // imgタグを作成してaタグに追加
+    const img = document.createElement('img');
+    img.src = './images/falhongcha-logo.png'; // 画像のURLを設定
+    img.alt = 'サークルロゴ'; // 代替テキストを設定
+    img.style.width = '150px';
+    a.appendChild(img);
+
+    // aタグをpタグに追加
+    p.appendChild(a);
+
+    // 最後のテキストノードを作成してpタグに追加
+    const text2 = document.createTextNode(' 2024');
+    p.appendChild(text2);
+
+    footerElement.appendChild(p);
+
+});
