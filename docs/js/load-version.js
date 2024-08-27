b465f52d9f011939bf6dce95dd6f3da56a29b04e
96ENU <96enu.illustration@gmail.com>
2024-08-27 18:00:29 +0900

    1.7.0仕様に更新しました。

--- docs/js/load-version.js
+++ docs/js/load-version.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const versionElements = document.querySelectorAll('#version');
     if (versionElements) {
         versionElements.forEach((versionElement) => {
-            versionElement.textContent = "Ver 1.6.9";
+            versionElement.textContent = "Ver 1.7.0";
         })
     }
 });
