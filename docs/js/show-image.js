43b5991b8c1a6c72961c9569d8aa15e8e47c61d3
96ENU <96enu.illustration@gmail.com>
2024-09-02 00:00:26 +0900

    表示されるスクリーンショットを角丸にする処理を追加しました。

--- docs/js/show-image.js
+++ docs/js/show-image.js
@@ -18,10 +18,15 @@ textElements.forEach(textElement => {
     // 画像の位置をテキストの上に設定
     imageElement.style.top = `${rect.top + scrollTop - imageElement.offsetHeight}px`; // テキストの上に画像を表示
     imageElement.style.left = `${rect.left + scrollLeft}px`; // テキストの左端に合わせる
+
+    // 徐々に画像を表示（透明度を0から1に変更）
+    imageElement.style.opacity = '1';
   });
 
   textElement.addEventListener('mouseout', () => {
     // マウスが離れた時に画像を非表示にする
     imageElement.style.display = 'none';
+    // 徐々に画像を非表示（透明度を1から0に変更）
+    imageElement.style.opacity = '0';
   });
 });
