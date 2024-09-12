9c9ce12664c23d8762dd5771dccaf19b9a5d59b9
96ENU <96enu.illustration@gmail.com>
2024-09-13 04:20:15 +0900

    ログ追加

--- docs/js/auto-open-details.js
+++ docs/js/auto-open-details.js
@@ -1,20 +1,22 @@
 // ページが読み込まれたときに実行される関数
-window.onload = function() {
-  // URLのハッシュ部分を取得
-  const hash = window.location.hash;
+window.onload = function () {
+    // URLのハッシュ部分を取得
+    const hash = window.location.hash;
 
-  // ハッシュ部分が空でない場合
-  if (hash) {
-    // ハッシュに対応する要素を取得
-    const targetElement = document.getElementById(hash.substring(1));
+    // ハッシュ部分が空でない場合
+    if (hash) {
+        // ハッシュに対応する要素を取得
+        const targetElement = document.getElementById(hash.substring(1));
 
-    // 要素が見つかった場合
-    if (targetElement) {
-      // 要素のsummary要素を取得
-      const summaryElement = targetElement.querySelector('summary');
+        // 要素が見つかった場合
+        if (targetElement) {
+            // 要素のsummary要素を取得
+            const summaryElement = targetElement.querySelector('summary');
 
-      // summary要素をクリックしてdetailsを開く
-      summaryElement.click();
+            // summary要素をクリックしてdetailsを開く
+            summaryElement.click();
+        } else {
+            console.error('not found');
+        }
     }
-  }
 };
