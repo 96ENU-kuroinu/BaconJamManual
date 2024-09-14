d94f42d4a45b087f294e2de6ef1496143d25e2ff
96ENU <96enu.illustration@gmail.com>
2024-09-15 05:16:49 +0900

    バージョン履歴のカテゴリを表示するデザインを変更しました。

--- docs/js/generate-version-hint.js
+++ docs/js/generate-version-hint.js
@@ -7,20 +7,36 @@ allDetails.forEach(details => {
     const summary = details.querySelector('.summary');
 
     // 各hr要素を取得
-    const newHr = details.querySelector('.new');
-    const changeHr = details.querySelector('.change');
-    const fixHr = details.querySelector('.fix');
+    const newHr     = details.querySelector('.new');
+    const changeHr  = details.querySelector('.change');
+    const fixHr     = details.querySelector('.fix');
 
     // 表示用のシンボルを格納する変数
     let symbols = '';
 
-    // hrタグの次に要素があればシンボルを追加
-    if (newHr.nextElementSibling && newHr.nextElementSibling.tagName !== 'HR') symbols += '<span style="color: green;">●</span>';
-    if (changeHr.nextElementSibling && changeHr.nextElementSibling.tagName !== 'HR') symbols += '<span style="color: orange;">●</span>';
-    if (fixHr.nextElementSibling && fixHr.nextElementSibling.tagName !== 'HR') symbols += '<span style="color: red;">●</span>';
+    // 丸のコンテナのスタイル
+    const containerStyle = `
+        display: inline-flex; /* inline-blockからinline-flexに変更して、要素をフレックスボックスで揃える */
+        align-items: center; /* 縦方向のセンタリング */
+        background-color: ghostwhite;
+        padding: 0 3px; /* 上下のpaddingを0にして左右だけにする */
+        border-radius: 20px;
+        margin-left: 10px;
+        box-shadow: 4px 4px rgba(0, 0, 0, 0.5);
+        line-height: 1; /* 行の高さを1にして余白を減らす */
+        float: right; /* 右寄せ */
+    `;
 
-    // シンボルがあればsummaryに追加
+    // 各シンボルのスタイル
+    const circleStyle = 'margin-right: 2px; font-size: 1.5em;';
+
+    // hrタグの次に要素があればシンボルを追加
+    symbols += `<span style="${circleStyle} color: ${newHr.nextElementSibling       && newHr.nextElementSibling.tagName     !== 'HR' ? 'green'  : 'black'};">●</span>`;
+    symbols += `<span style="${circleStyle} color: ${changeHr.nextElementSibling    && changeHr.nextElementSibling.tagName  !== 'HR' ? 'orange' : 'black'};">●</span>`;
+    symbols += `<span style="${circleStyle} color: ${fixHr.nextElementSibling       && fixHr.nextElementSibling.tagName     !== 'HR' ? 'red'    : 'black'};">●</span>`;
+
+    // シンボルを1つの背景で囲んで追加
     if (symbols) {
-        summary.innerHTML += ' ' + symbols;
+        summary.innerHTML += ` <div style="${containerStyle}">${symbols}</div>`;
     }
 });
