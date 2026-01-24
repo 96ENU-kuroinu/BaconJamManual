d5c3557508242a327a7ed7afb5bc9e71da5cc8f6
96ENU <96enu.illustration@gmail.com>
2026-01-25 00:16:28 +0900

    リダイレクトに変更して不要になったファイルを削除

--- docs/js/load-version.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const versionElements = document.querySelectorAll('#version');
-    if (versionElements) {
-        versionElements.forEach((versionElement) => {
-            versionElement.textContent = "Ver 1.11.1";
-        })
-    }
-});
